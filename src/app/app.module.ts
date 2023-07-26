49c1f71f352ab3083c6b524a586b7d61a27b16a8
Guilherme França da Silva <francaguilherme27@gmail.com>
2023-07-26 10:39:07 -0300

    Creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuemSomosComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
