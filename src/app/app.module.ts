6e58235c3980df1d8fc118bec844c6c5a6edb399
Guilherme França da Silva <francaguilherme27@gmail.com>
2023-08-02 17:07:48 -0300

    Criação do rodape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { DetalhesComponent } from './detalhes/detalhes.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { DetalhesComponent } from './detalhes/detalhes.component';
     QuemSomosComponent,
     ContatoComponent,
     CabecalhoComponent,
-    DetalhesComponent
+    DetalhesComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
