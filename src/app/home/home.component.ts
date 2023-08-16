a8a6fc13c1fc3b1f5252d0a9681abf5a33b894c6
Guilherme França da Silva <francaguilherme27@gmail.com>
2023-08-16 17:00:38 -0300

    patch favoritos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarModule,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
@@ -15,7 +21,8 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
     MatGridListModule,
     MatCardModule,
     MatIconModule,
-    CommonModule
+    CommonModule,
+    MatSnackBarModule
   ]
 })
 export class HomeComponent implements OnInit {
@@ -23,20 +30,53 @@ export class HomeComponent implements OnInit {
   nome: string = 'Guilherme';
   sobrenome: string = 'França';
   imoveis: any;
+  imovel: any;
 
-  constructor(private router: Router, private http: HttpClient) { }
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+
+  constructor(private router: Router, private http: HttpClient, private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
-      this.http.get<any>('http://localhost:3000/imoveis').subscribe(data => {
-        this.imoveis = data;
-      });
+    this.http.get<any>('http://localhost:3000/imoveis').subscribe(data => {
+      this.imoveis = data;
+    });
   }
 
   verDetalhes(imovelId: string) {
     this.router.navigate(['/detalhes', imovelId]);
   }
 
-  toogleFavorito(index: number): void {
-    this.imoveis[index].favorito = !this.imoveis[index].favorito;
+  toogleFavorito(imovelId: number): void {
+    this.http.get<any>('http://localhost:3000/imoveis/' + imovelId).subscribe(data => {
+      this.imovel = data;
+      this.imovel.favorito = !this.imovel.favorito;
+
+      this.http.patch('http://localhost:3000/imoveis/' + imovelId, { favorito: this.imovel.favorito })
+        .subscribe(
+          response => {
+            if (this.imovel.favorito) {
+              this._snackBar.open('O imóvel foi favoritado', 'Fechar', {
+                horizontalPosition: this.horizontalPosition,
+                verticalPosition: this.verticalPosition,
+                duration: 5000
+              });
+            } else {
+              this._snackBar.open('Imóvel removido dos favoritos', 'Fechar', {
+                horizontalPosition: this.horizontalPosition,
+                verticalPosition: this.verticalPosition,
+                duration: 5000
+              });
+            }
+            this.http.get<any>('http://localhost:3000/imoveis').subscribe(data => {
+              this.imoveis = data;
+            });
+          },
+          error => {
+            console.log("Ocorreu um erro!");
+            this.imovel.favorito = !this.imovel.favorito;
+          }
+        );
+    });
   }
 }
